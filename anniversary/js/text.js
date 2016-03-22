26ad601a4ac2d2c2ea55b71b57131dc821847b51
Prashanth <prashanth1395@gmail.com>
2016-03-22 23:57:46 +0530

    Removing anniversary

--- anniversary/js/text.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $(".text").typed({
-    strings: ["Hi Shikha!","Oh my dear lovely Princess!", "It's been a beautiful 1 year since we are in love with each other.", "I can never forget this day!", "Whenever I see you, I'll be like....!!! ^4500", "I want to spend my whole life looking at you.","You are my darling!!!", "And, ^500 my dear darling... ^1200 I Love You So much...!!! :*"],
-    typeSpeed: 0
-  });
-});
