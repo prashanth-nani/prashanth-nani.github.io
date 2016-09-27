fa44f9fe10c81be8b3f9556706602c1f814e9392
Prashanth <prashanth1395@gmail.com>
2016-09-27 23:03:15 +0530

    Add layDown to all sections

--- test/js/functions.js
+++ test/js/functions.js
@@ -4,13 +4,22 @@ function hamburgerHandler(toggle) {
     if (this.classList.contains("is-active") === true){
       this.classList.remove("is-active");
       document.querySelector('#nav-overlay').style.width = '0%';
-      document.querySelector('#main-container').className = '';
+      // section[secNum].className = 'section';
+      $( ".section" ).each(function() {
+          $( this ).removeClass( "layDown" );
+        });
+      // document.querySelector('#container').className = '';
       document.querySelector('body').style.overflow = 'auto';
     }
     else{
         this.classList.add("is-active");
         document.querySelector('#nav-overlay').style.width = '100%';
-        document.querySelector('#main-container').className = 'animate';
+        document.querySelector('#container').className = 'perspective';
+        secNum = getNextSectionNumber() - 1;
+        //section[secNum].className = 'layDown section';
+        $( ".section" ).each(function() {
+            $( this ).addClass( "layDown" );
+          });
         document.querySelector('body').style.overflow = 'hidden';
     }
   });
@@ -19,9 +28,23 @@ function hamburgerHandler(toggle) {
 function scrollBtnHandler(scroll){
   scroll.addEventListener("click", function(e){
     e.preventDefault();
-
-    $('html,body').animate({
-        scrollTop: $("#portfolio").offset().top},
-        'slow');
+    var i = getNextSectionNumber();
+    scrollTo(section[i]);
   });
+}
+
+function scrollTo(element) {
+  $('html,body').animate({
+      scrollTop: element.offsetTop},
+      'slow');
+}
+
+function getNextSectionNumber(){
+  var presentTop = $(window).scrollTop();
+  for(var i=0; i<section.length; i++)
+  {
+    if(section[i].offsetTop > presentTop )
+      break;
+  }
+  return i;
 }
--- test/js/scripts.js
+++ test/js/scripts.js
@@ -1,5 +1,16 @@
-$("#scroll-btn").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#portfolio").offset().top},
-        'slow');
+var section = document.getElementsByClassName('section');
+var secNum;
+
+$(document).ready(function(){
+  "use strict";
+
+  var navul = document.getElementById('nav-bar').children[0].cloneNode(true);
+  document.getElementById('nav-overlay').appendChild(navul);
+
+  var toggle = document.querySelector(".c-hamburger");
+  hamburgerHandler(toggle);
+
+  var scroll = document.getElementById('scroll-btn');
+  scrollBtnHandler(scroll, section);
+
 });
